d1e6f6f0d674576c05d8e2f717398ee2226a7098
Null <kelanidarasimi9@gmail.com>
2024-06-06 13:50:41 +0100

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from typing import Tuple, Union
+
+"""
+return a tuple of key and value pair
+"""
+
+
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    """
+    return a tuple of values
+    """
+    return (k, v)
