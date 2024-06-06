ae3b4e520d32938aa1400918abf877e88c9a5b44
Null <kelanidarasimi9@gmail.com>
2024-06-06 13:25:13 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+return a sum of list
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    return a sum of float list
+    """
+    return sum(input_list)
