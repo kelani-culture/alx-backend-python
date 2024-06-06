436445a59ed7efc95a1d4a25a0ea3b860eab2d4c
Null <kelanidarasimi9@gmail.com>
2024-06-06 13:56:59 +0100

    task 7 redid

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from typing import Tuple, Union
-
+from math import e
 """
 return a tuple of key and value pair
 """
@@ -10,4 +10,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     return a tuple of values
     """
-    return (k, v)
+    return (k, float(v ** 2))
