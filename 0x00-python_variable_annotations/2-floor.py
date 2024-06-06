e86eb89dc30d49a123ff8fe12892253bf421d3cf
Null <kelanidarasimi9@gmail.com>
2024-06-06 12:52:44 +0100

    task 3

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -8,4 +8,7 @@ and returns the floor of the float.
 
 
 def floor(n: float) -> int:
+    """
+    return the floor value of n
+    """
     return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+convert to string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    return the string representation of n
+    """
+    return str(n)
