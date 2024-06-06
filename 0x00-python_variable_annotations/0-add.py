55861b87d4518c5375e60a78ce40895f388551e1
Null <kelanidarasimi9@gmail.com>
2024-06-06 10:54:50 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+add too numeric values together to product an output
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    return the added value of both num1 and num2
+    """
+    return a + b
