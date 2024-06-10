bcc2508b2fce9006b729b63953a76df741b5512a
Null <kelanidarasimi9@gmail.com>
2024-06-10 15:20:25 +0100

    task 4 redid

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,7 +6,7 @@ the task_wait_n function documentation
 import asyncio
 from typing import List
 
-task_wait_random = __import__('3-tasks').task_wait_random
+task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
