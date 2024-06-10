8a6937b0db1b44f24fcdcd608a1d2b12a41ba480
Null <kelanidarasimi9@gmail.com>
2024-06-10 15:05:36 +0100

    task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+asyncio task documentation
+"""
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+    return asyncio task doc
+    """
+    a = asyncio.create_task(wait_random(max_delay))
+    return a
