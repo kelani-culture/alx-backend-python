d8d5ee3b38d6e37d172efd15684df220d7a4e1be
Null <kelanidarasimi9@gmail.com>
2024-07-01 21:42:40 +0100

    task1 redid

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 unitest on utilities function
 """
 import unittest
+from typing import Mapping, Tuple, Union
 
 from parameterized import parameterized  # type: ignore
 
@@ -16,11 +17,13 @@ class TestAccessNestedMad(unittest.TestCase):
 
     @parameterized.expand(
         [
-            ("test_map_1", {"a": 1}, ("a",), 1),
-            ("test_map_2", {"a": {"b": 1}}, ("a",), {'b': 1}),
-            ("test_map_3", {"a": {"b": 2}}, ("a", "b"), 2),
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 1}}, ("a",), {"b": 1}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
         ]
     )
-    def test_access_nested_map(self, name, nested_map, path, expected):
-        """ the test access nested map unit test function"""
+    def test_access_nested_map(
+        self, nested_map: Mapping, path: Tuple, expected: Union[Mapping, int]
+    ):
+        """the test access nested map unit test function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
